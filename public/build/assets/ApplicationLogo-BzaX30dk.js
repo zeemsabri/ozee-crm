be4f11f4c0091317d63053de63b0026cfa39e5c9
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2025-08-03 13:33:43 +0800

    new build

--- public/build/assets/ApplicationLogo-BzaX30dk.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,i as c,o as e}from"./app-BiQ7j4uU.js";const t={},n={height:"1200",src:"/logo.png",alt:"OZEE Logo"};function r(s,_){return e(),c("img",n)}const i=o(t,[["render",r]]);export{i as A};
