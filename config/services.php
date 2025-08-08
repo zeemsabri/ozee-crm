3eedbae07a97547946685118d21daa5cf1bd15fc
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2025-08-08 17:44:55 +0800

    updated Google OAuth services config to add default values for user_redirect and primary_email settings

--- config/services.php
+++ config/services.php
@@ -39,8 +39,8 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
-        'user_redirect' =>  env('GOOGLE_USER_REDIRECT_URI'),
-        'primary_email' =>  env('GOOGLE_PRIMARY_EMAIL'),
+        'user_redirect' =>  env('GOOGLE_USER_REDIRECT_URI', env('APP_URL').'/google/usercallback'),
+        'primary_email' =>  env('GOOGLE_PRIMARY_EMAIL', 'info@ozeeweb.com.au'),
     ],
 
     'exchange_rates' => [
