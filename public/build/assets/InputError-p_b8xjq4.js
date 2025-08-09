e59fb8649d1979dcd4d0698212fa75e52adf2d17
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2025-08-09 16:15:37 +0800

    adjusted client visibility filter handling in ProjectClientReader and ShareableResourceController for consistency

--- public/build/assets/InputError-p_b8xjq4.js
+++
@@ -1,1 +0,0 @@
-import{f as s,W as t,i as a,o as r,b as o,t as c}from"./app-xL9RQNW-.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
