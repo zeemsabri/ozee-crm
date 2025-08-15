16ca0c3bd358a5ee4782871d0019f1ead7666b58
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2025-08-15 11:59:01 +0800

    deleted

---
+++ public/build/assets/InputLabel-3a7HbWEV.js
@@ -0,0 +1,1 @@
+import{a as e,o as t,t as o,N as n}from"./app-Cunc5QWV.js";const l={class:"block text-sm font-medium text-gray-700"},r={key:0},c={key:1},p={__name:"InputLabel",props:{value:{type:String}},setup(s){return(a,_)=>(t(),e("label",l,[s.value?(t(),e("span",r,o(s.value),1)):(t(),e("span",c,[n(a.$slots,"default")]))]))}};export{p as _};
