36a7afbe3b71a2d15fbac064fc232626f2efc91d
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2025-10-09 10:32:34 +0800

    updated

--- public/build/assets/InputError-DbJd9iC5.js
+++
@@ -1,1 +0,0 @@
-import{v as s,K as t,a,o as r,b as o,t as c}from"./app-CDr3a6VS.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
