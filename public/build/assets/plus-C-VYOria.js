4cb0886c69a6edb10ce9f0c28d3a87bda9841d2b
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2026-02-14 08:48:24 +0800

    latest build

---
+++ public/build/assets/plus-C-VYOria.js
@@ -0,0 +1,6 @@
+import{c as e}from"./createLucideIcon-DBeR4H4x.js";/**
+ * @license lucide-vue-next v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);export{a as P};
