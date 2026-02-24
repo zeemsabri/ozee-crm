062f5921020394d40694cf7331f6dac5e304681e
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2026-02-25 05:22:38 +0800

    latest build

---
+++ public/build/assets/TabPanel-CW3QX1Cn.js
@@ -0,0 +1,1 @@
+import{f as t,L as o,o as s,c as l,Z as r}from"./app-BB49X1tp.js";const i={__name:"TabPanel",props:{active:{type:Boolean,default:!1}},setup(e){return(a,c)=>t((s(),l("div",null,[r(a.$slots,"default")],512)),[[o,e.active]])}};export{i as default};
