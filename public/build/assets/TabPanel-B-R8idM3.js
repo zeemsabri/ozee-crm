6b6c4c952011935ce2922fd75e2d24778d808182
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2026-03-02 06:11:34 +0800

    latest build

---
+++ public/build/assets/TabPanel-B-R8idM3.js
@@ -0,0 +1,1 @@
+import{i as t,K as o,o as s,c as l,Y as r}from"./app-uPy8Yanj.js";const i={__name:"TabPanel",props:{active:{type:Boolean,default:!1}},setup(e){return(a,c)=>t((s(),l("div",null,[r(a.$slots,"default")],512)),[[o,e.active]])}};export{i as default};
