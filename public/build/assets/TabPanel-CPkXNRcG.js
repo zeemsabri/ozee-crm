968607b44d5682e573308a4f0c6d3259f777749c
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2026-03-13 11:32:02 +0800

    latest build

---
+++ public/build/assets/TabPanel-CPkXNRcG.js
@@ -0,0 +1,1 @@
+import{i as t,K as o,o as s,c as l,$ as r}from"./app-D_8mAW2h.js";const i={__name:"TabPanel",props:{active:{type:Boolean,default:!1}},setup(e){return(a,c)=>t((s(),l("div",null,[r(a.$slots,"default")],512)),[[o,e.active]])}};export{i as default};
