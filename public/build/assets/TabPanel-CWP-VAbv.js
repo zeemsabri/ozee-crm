9ddd660d18ef54dd0c9b0aaff436dc39497f100e
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2026-07-29 08:25:31 +0800

    latest build

---
+++ public/build/assets/TabPanel-CWP-VAbv.js
@@ -0,0 +1,1 @@
+import{g as t,O as o,o as s,c as l,a1 as r}from"./app-7ApnOe0z.js";const i={__name:"TabPanel",props:{active:{type:Boolean,default:!1}},setup(e){return(a,c)=>t((s(),l("div",null,[r(a.$slots,"default")],512)),[[o,e.active]])}};export{i as default};
