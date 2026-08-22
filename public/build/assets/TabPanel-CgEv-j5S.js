51b9c6fcbc53495ce4f727c599cd821f5f31cfd8
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2026-08-23 07:37:08 +0800

    latest build

---
+++ public/build/assets/TabPanel-CgEv-j5S.js
@@ -0,0 +1,1 @@
+import{g as t,N as o,o as r,c as s,a1 as l}from"./vue-entry-C11vZSyV.js";import"./app-DTy8mHo2.js";import"./index-DrGCbs5E.js";const m={__name:"TabPanel",props:{active:{type:Boolean,default:!1}},setup(e){return(a,c)=>t((r(),s("div",null,[l(a.$slots,"default")],512)),[[o,e.active]])}};export{m as default};
